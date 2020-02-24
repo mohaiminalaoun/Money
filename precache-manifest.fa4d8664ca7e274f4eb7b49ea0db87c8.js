40668b7f58b6e66fed96755c930371b41a7d07b8
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2020-02-23 20:29:04 -0500

    Updates

---
+++ precache-manifest.fa4d8664ca7e274f4eb7b49ea0db87c8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb2344e6e4a6ee394b5898a1bac2fcb",
+    "url": "/money/index.html"
+  },
+  {
+    "revision": "36e3c32df864f8d4e971",
+    "url": "/money/static/css/2.78f95891.chunk.css"
+  },
+  {
+    "revision": "0a23e9e4d7742f766ab9",
+    "url": "/money/static/css/main.e1508efe.chunk.css"
+  },
+  {
+    "revision": "36e3c32df864f8d4e971",
+    "url": "/money/static/js/2.61d0ddfd.chunk.js"
+  },
+  {
+    "revision": "209d7f261911842216918882d1f3ddbc",
+    "url": "/money/static/js/2.61d0ddfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a23e9e4d7742f766ab9",
+    "url": "/money/static/js/main.73c04d1e.chunk.js"
+  },
+  {
+    "revision": "4ccaf293d04f39c4da99",
+    "url": "/money/static/js/runtime-main.31a8cf7b.js"
+  },
+  {
+    "revision": "9745415b8a2a8b9a2480a12f7e2d3ad3",
+    "url": "/money/static/media/Roboto-Medium.9745415b.ttf"
+  },
+  {
+    "revision": "14147ca3288fa08b850d029193150722",
+    "url": "/money/static/media/Roboto-Thin.14147ca3.ttf"
+  },
+  {
+    "revision": "f41441d75c5ef6ed044a998fa70f462b",
+    "url": "/money/static/media/back.f41441d7.svg"
+  },
+  {
+    "revision": "0e1d7d58df7f1757ffad6707eedf9be9",
+    "url": "/money/static/media/backspace.0e1d7d58.svg"
+  },
+  {
+    "revision": "94502f89460200386d25c6b84c841b83",
+    "url": "/money/static/media/calendar.94502f89.svg"
+  },
+  {
+    "revision": "7815fcb3a59a234da50bab4514f6bb9c",
+    "url": "/money/static/media/confirm.7815fcb3.svg"
+  },
+  {
+    "revision": "6c7f3dc284d55fb23e943a6588788cab",
+    "url": "/money/static/media/google-icon-2.6c7f3dc2.svg"
+  },
+  {
+    "revision": "ec85f9445075197744c6a2f73f61d3e3",
+    "url": "/money/static/media/google-icon.ec85f944.svg"
+  },
+  {
+    "revision": "478c67c0103950deef644cc554a24114",
+    "url": "/money/static/media/more.478c67c0.svg"
+  },
+  {
+    "revision": "d7adac28ad5a1f165610500f48450529",
+    "url": "/money/static/media/price.d7adac28.svg"
+  }
+]);
